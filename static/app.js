77342f1cff1dbabf40180782d49e4163ff834521
Jason Matthias <jpmatthias90@gmail.com>
2019-04-25 15:58:10 -0700

    moved liking messages to front end

--- static/app.js
+++ static/app.js
@@ -1,8 +1,12 @@
 $(function () {
     function handleMessageDelete(res) {
-
         //remove li from dom
         $(`#msg-${res}`).remove()
+    }
+    
+    function handleLikeMessage(res) {
+        $(`#like-${res.msgId}`).toggleClass("fas far")
+        $(`#${res.msgId}-num-likes`).text(`${res.likes}`)
     }
 
     $('[data-toggle="tooltip"]').tooltip()
@@ -19,6 +23,26 @@ $(function () {
             type: "POST",
             url: `/messages/${msg_id}/delete`,
             success: handleMessageDelete
+        })
+    })
+
+    $('.like-btn').on('click', (e) => {
+        e.preventDefault()
+
+        //get message_id
+        msg_id = e.target.getAttribute('data-msg')
+        
+        data = {
+            "msg-id": msg_id
+        }
+ 
+        //post request to like end point
+        $.ajax({
+            type: "POST",
+            url: `/like`,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: handleLikeMessage
         })
     })
 })
